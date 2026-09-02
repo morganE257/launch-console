572ab4be9a4aa964dcf037e9601e2e3831bf9a37
morganE257 <morganeggert@gmail.com>
2026-09-02 01:58:16 +0000

    added new functions

--- launch_console.py
+++ launch_console.py
@@ -1,3 +1,29 @@
 print("Welcome to the Launch Console!")
+
 name = 'Morgan'
 print("Hi, " + name + "!")
+
+menu = ["About me", "My goals", "Exit"]
+print(len(menu))
+
+def about_me(name):
+    return "My name is " + name + "."
+    pass
+
+print(about_me("Ada"))
+
+choice = "3"
+if choice == "3":
+    print("Goodbye!")
+
+running = True
+while running:
+    print("running...")
+    running = False
+
+def mystery(words):
+    out = ""
+    for w in words:
+        out = out + w[0]
+    return out
+print(mystery(["Grit", "Impact", "Trust"]))
